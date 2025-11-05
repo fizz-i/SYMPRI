30566b58573262928773d4ce2a81a0c69fdc6a22
gautam punj <gautam.010.021@gmail.com>
2025-11-06 00:26:30 +0530

    database made bug fixes

--- database/main.py
+++ database/main.py
@@ -5,9 +5,9 @@ import pandas as pd
 conn = sqlite3.connect('database.db')
 
 # Read CSV files and write to SQLite tables
-precautions_df = pd.read_csv('/home/fizzi/Documents/SYMPRI/database/datasets/precautions(1).csv')
-medications_df = pd.read_csv('/home/fizzi/Documents/SYMPRI/database/datasets/medications(1).csv')
-symptoms_df = pd.read_csv('/home/fizzi/Documents/SYMPRI/database/datasets/symptoms(1).csv')
+precautions_df = pd.read_csv('/home/fizzi/Documents/SYMPRI/database/datasets/precautions(1).csv',index_col=0)
+medications_df = pd.read_csv('/home/fizzi/Documents/SYMPRI/database/datasets/medications(1).csv', index_col=0)
+symptoms_df = pd.read_csv('/home/fizzi/Documents/SYMPRI/database/datasets/symptoms(1).csv', index_col=0)
 
 precautions_df.to_sql('precautions', conn, if_exists='replace', index=False)
 medications_df.to_sql('medications', conn, if_exists='replace', index=False)
